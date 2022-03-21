14b06a50369effc3585dc54c2918d2e430b34621
nj <niklas@julit.dk>
2022-03-21 13:13:52 +0100

    Add project files.

---
+++ Gæt et tal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Gæt_et_tal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100);
+            int i = 0;
+            Console.WriteLine("Guess the number");
+            int guess = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (guess == randomNumber)
+                {
+                    Console.WriteLine("Congrats you gussed it in " + i + " tries");
+                    break;
+                }
+                else if (guess >= randomNumber)
+                {
+                    Console.WriteLine("You guess was too high");
+                    i++;
+                    guess = int.Parse(Console.ReadLine());
+                }
+                else if (guess <= randomNumber)
+                {
+                    Console.WriteLine("You guess was too low");
+                    i++;
+                    guess = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
